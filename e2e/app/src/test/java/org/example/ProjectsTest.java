af091a0788b0f72730b61a7a88b9ee40f2522698
Neil Studd <neil.studd@makers.tech>
2024-05-07 14:18:06 +0100

    see if we can avoid having to be headless

--- e2e/app/src/test/java/org/example/ProjectsTest.java
+++ e2e/app/src/test/java/org/example/ProjectsTest.java
@@ -22,7 +22,7 @@ public class ProjectsTest {
         WebDriverManager.chromedriver().setup();
 
         ChromeOptions options = new ChromeOptions();
-        options.addArguments("--headless", "--no-sandbox", "--disable-dev-shm-usage");
+        options.addArguments("--remote-debugging-pipe");
         driver = new ChromeDriver(options);
         System.out.println(driver.getCapabilities().getBrowserVersion());
 
