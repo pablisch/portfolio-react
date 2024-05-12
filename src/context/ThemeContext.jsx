ecc35efcead7fc2fa3b2c43e1ef52e5ef17cbd98
Pablo <pablito@me.com>
2024-05-12 10:29:44 +0100

    Added provider and custom hook without implementation.

--- src/context/ThemeContext.jsx
+++ src/context/ThemeContext.jsx
@@ -1,5 +1,28 @@
-import { createContext } from "react";
+import { createContext, useState, useContext } from "react";
+import PropTypes from "prop-types";
 
 const ThemeContext = createContext()
+
+export const ThemeProvider = ({ children }) => {
+  const [theme, setTheme] = useState('light');
+
+  return (
+    <ThemeContext.Provider value={{ theme, setTheme }}>
+      {children}
+    </ThemeContext.Provider>
+  );
+}
+
+ThemeProvider.propTypes = {
+  children: PropTypes.node.isRequired,
+};
+
+export const useTheme = () => {
+  const context = useContext(ThemeContext);
+  if (!context) {
+    throw new Error('useTheme must be used within a ThemeProvider');
+  }
+  return context;
+}
 
 export default ThemeContext;
