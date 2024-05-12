fefdcfb1f12f5e854e94ec67587e1604cb6f2a16
Pablo <pablito@me.com>
2024-05-12 21:18:34 +0100

    Moved all theme logic into themeContext.

--- src/context/ThemeContext.jsx
+++ src/context/ThemeContext.jsx
@@ -1,13 +1,27 @@
 import { createContext, useState, useContext } from "react";
 import PropTypes from "prop-types";
 
+const themeStyles = ['retro', 'light', 'dark'];
+
 const ThemeContext = createContext()
 
 export const ThemeProvider = ({ children }) => {
   const [theme, setTheme] = useState('light');
+  const [isIconRotating, setIsIconRotating] = useState(false); // for rotating theme icon animation
+
+  const onThemeChange = () => {
+    const themeIndex = themeStyles.indexOf(theme);
+    const newThemeIndex =
+      themeIndex === themeStyles.length - 1 ? 0 : themeIndex + 1;
+    setTheme(themeStyles[newThemeIndex]);
+    setIsIconRotating(true);
+    setTimeout(() => {
+      setIsIconRotating(false);
+    }, 500);
+  };
 
   return (
-    <ThemeContext.Provider value={{ theme, setTheme }}>
+    <ThemeContext.Provider value={{ theme, onThemeChange, isIconRotating }}>
       {children}
     </ThemeContext.Provider>
   );
